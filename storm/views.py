d874f5fb79f069eb44b0f10ccaca4fe29451ca60
Josip Šušak <josip.susak1993@gmail.com>
2023-03-01 19:45:31 +0100

    Added Storm class and functionality to Storm app

--- storm/views.py
+++ storm/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from random import randint
 
-# Create your views here.
+
+class Storm:
+    def __init__(self):
+        self.chance_to_hit = randint(1, 4)
+
+    def hit_ship(self, ship_hp):
+        damage_done_to_ship = ship_hp * (self.chance_to_hit / 100)
+        return damage_done_to_ship
+
+    def hit_soldiers(self, soldier_hp):
+        damage_done_to_soldier = soldier_hp * (self.chance_to_hit / 100)
+        return damage_done_to_soldier
