dbfd89b69a5c9ec3fabbd98a35a753ad946ff138
Josip Šušak <josip.susak1993@gmail.com>
2023-03-01 23:07:41 +0100

    Added Famine functionality

--- famine/views.py
+++ famine/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from random import randrange
 
-# Create your views here.
+
+class Famine:
+    def __init__(self):
+        self.chance_to_hit = randrange(10, 100, 10)
+
+    def hit_soldiers(self, soldiers):
+        damage_done_to_soldier = soldiers * (self.chance_to_hit / 100)
+        return damage_done_to_soldier
