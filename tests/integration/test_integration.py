d778c9d5b2cf1ae6df2644dd9b3087fc7b1e8e34
Josip Šušak <josip.susak1993@gmail.com>
2023-03-05 19:41:28 +0100

    Added comments in integration tests

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -1,10 +1,13 @@
 class TestIntegration:
 
+    # Tests if get method is working, asserting "Attention" as it's only in response when get method is valid
     def test_get(self, client):
         response = client.get("http://127.0.0.1:8000/")
         assert response.status_code == 200
         assert "Attention" in response.data
 
+    # Tests if app is working when proper query parameters are input, asserting "Winner is", because it's only inside of
+    # response if app is working properly and results are calculated
     def test_query_parameters_insert(self, client):
         response = client.get("http://127.0.0.1:8000/?ships=50,20,70")
         assert response.status_code == 200
